e5582d592a0c1a9810b7224e408926e48f71385a
xiaolong.mxl <857821767@qq.com>
2018-07-06 15:23:00 +0800

    web add

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+alert('Yay, I love chocolate ice cream!');
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
